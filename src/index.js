75b799f9bbc48bfe432a1949fcd8738dbe07ce2b
Matti Cone <mcone@uoregon.edu>
2018-03-13 08:40:09 -0700

    integrated suppliers responses

--- src/index.js
+++ src/index.js
@@ -16,12 +16,6 @@ console.log(config.version);
 let route = '/acme/api/'+config.version;
 app.use(route, order({config}));
 
-// app.post('/hello', function(req, res) {
-//
-//     console.log("hello variable: ", req.query.hello);
-//     res.status(200).json({'recieved ': req.query.hello});
-// });
-
 app.server.listen(config.port);
 console.log(`Started on port ${app.server.address().port}`);
 
